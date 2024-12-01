b0f96943c2e5f883993fd480f51ab2f00b495a3e
hih0lp <mrlibobo@yandex.ru>
2024-12-01 19:35:51 +0300

    simple parser

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+string filePath = AppDomain.CurrentDomain.BaseDirectory + '\\';
+IWebDriver driver = new ChromeDriver(filePath);
+
+driver.Navigate().GoToUrl("https://www.coffee-anytime.ru/");
+Thread.Sleep(5000);
+
+Console.WriteLine(1);
+
+List<IWebElement> elements = driver.FindElements(By.XPath(
+    "//div[@class='tab-pane products is-tab active']" +
+    "/div[@class='products__item']" +
+    "/p[@class='products__name']")).ToList();
+
+Console.WriteLine(1);
+
+List<string> prices = new List<string>();
+
+Console.WriteLine(1);
+
+foreach (IWebElement elem in elements)
+{
+    prices.Add(elem.Text);
+    Console.WriteLine(1);
+}
+
+for (int i = 0; i < prices.Count; i++)
+{
+    Console.Write(prices[i]);
+}
